c4000b494bd6d3ee15c6db9d68e5c428f847a3fd
kulikp1 <pashakylik2@gmail.com>
2025-03-17 22:31:18 +0200

    create regoinClick util

---
+++ src/utils/regionClick.js
@@ -0,0 +1,26 @@
+const regionNames = {
+  path6026: "Київська",
+  path5946: "Сумська",
+  path5958: "Харківська",
+  path6028: "Чернігівська",
+  path6034: "Луганська",
+  path5966: "Донецька",
+  path7: "Кримська",
+  path5994: "Одеська",
+  path5924: "Закарпатська",
+  path3616: "Полтавська",
+  path5970: "Дніпропетровська",
+  path5876: "Запорізька",
+  path3316: "Херсонська",
+  path6016: "Миколаївська",
+  path3404: "Кіровоградська"
+};
+
+const handleRegionClick = (event) => {
+  const region = event.target.id;
+  if (regionNames[region]) {
+    console.log(`Натиснута область: ${regionNames[region]}`);
+  }
+};
+
+export default handleRegionClick;
