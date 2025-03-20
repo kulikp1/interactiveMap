21d9a79ef75f3a057905119b49f769fec7194288
kulikp1 <pashakylik2@gmail.com>
2025-03-20 18:53:36 +0200

    add import data example population for modal from .json file

--- src/Modal/RegionModal.jsx
+++ src/Modal/RegionModal.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Modal from "react-modal";
 import style from "./RegionModal.module.css";
-
-// Modal.setAppElement("#root"); // Вказуємо головний контейнер програми
+import regionsData from "../utils/regionsData.json"; // Імпортуємо JSON
 
 const RegionModal = ({ region, onClose }) => {
+  const regionInfo = region ? regionsData[region] : null;
+
   return (
     <Modal
       isOpen={!!region}
@@ -14,6 +15,11 @@ const RegionModal = ({ region, onClose }) => {
       overlayClassName={style.overlay}
     >
       <h2 className={style.region}>{region ? `Область: ${region}` : ""}</h2>
+      {regionInfo && (
+        <p className={style.population}>
+          Населення: {regionInfo.population.toLocaleString()} осіб
+        </p>
+      )}
       <button onClick={onClose} className={style.closeButton}>
         Закрити
       </button>
