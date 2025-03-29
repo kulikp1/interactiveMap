95a3e6595b9e216c7f58682ef5e7090d902cceb5
kulikp1 <pashakylik2@gmail.com>
2025-03-29 21:29:27 +0200

    add test content to statistic page

---
+++ src/components/statistic/statistic.jsx
@@ -0,0 +1,11 @@
+import css from "./statistic.module.css";
+
+const Statistic = () => {
+  return (
+    <div className={css.mainContainer}>
+      <h1>Statistic page</h1>
+    </div>
+  );
+};
+
+export default Statistic;
