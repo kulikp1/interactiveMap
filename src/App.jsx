274a802b36bbe14647553bee792d3df9ba51db20
kulikp1 <pashakylik2@gmail.com>
2025-03-29 21:32:47 +0200

    add route for Statistic

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import UkraineMap from "./components/Map/map";
+import Statistic from "./components/statistic/statistic";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<UkraineMap />} />
+        <Route path="/statistic" element={<Statistic />} />
       </Routes>
     </Router>
   );
