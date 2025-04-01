3199b136585196f0b440de2d1ef3f8c3b452b380
kulikp1 <pashakylik2@gmail.com>
2025-04-01 22:19:34 +0300

    clean statistic.js

--- src/components/statistic/statistic.jsx
+++ src/components/statistic/statistic.jsx
@@ -9,22 +9,6 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-// const data = [
-//   { name: "Населення", value: 2.9 },
-//   { name: "Туристи", value: 2 },
-//   { name: "Станції метро", value: 52 },
-//   { name: "Зелена зона (%)", value: 46 },
-// ];
-
-// const data = [
-//   { year: "2018", visitors: 1.9 },
-//   { year: "2019", visitors: 2.2 },
-//   { year: "2020", visitors: 1.3 },
-//   { year: "2021", visitors: 1.8 },
-//   { year: "2022", visitors: 2.1 },
-//   { year: "2023", visitors: 2.4 },
-// ];
-
 const Statistic = () => {
   return (
     <div className={css.mainContainer}>
